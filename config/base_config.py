fe3ad9db8cdcf719579c4a60f090d524a931acf6
LuckyBBBBB <2876740043@qq.com>
2026-01-18 15:33:57 +0800

    【调整】兼容uv命令不存在

--- config/base_config.py
+++ config/base_config.py
@@ -1,6 +1,6 @@
 # 基础配置
 PLATFORM = "xhs"  # 平台，xhs | zhihu | xhy
-KEYWORDS = "咖啡,美式"  # 关键词搜索配置，以英文逗号分隔
+KEYWORDS = "产品"  # 关键词搜索配置，以英文逗号分隔
 LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
 COOKIES = ""
 CRAWLER_TYPE = (
@@ -61,7 +61,7 @@ USER_DATA_DIR = "%s_user_data_dir"  # %s will be replaced by platform name
 START_PAGE = 1
 
 # 爬取视频/帖子的数量控制
-CRAWLER_MAX_NOTES_COUNT = 15
+CRAWLER_MAX_NOTES_COUNT = 1
 
 # 并发爬虫数量控制
 MAX_CONCURRENCY_NUM = 1
